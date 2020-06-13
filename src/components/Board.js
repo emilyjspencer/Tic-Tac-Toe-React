0bc5aeb42e8c0ccd9867edcf30551b2852d3297b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-13 14:31:29 +0100

    Adds styling to the Board component using inline styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Square from './Square';
 
+const styling = {
+  border: '10px',
+  borderRadius: '12px',
+  width: '350px',
+  height: '500px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
 
+}
 
 const Board = ({ squares, onClick }) => 
      (
-        <div>
+        <div style={styling}>
             <Square value = "1" onClick={() => onClick("test") } />
             <Square value = "2" onClick={() => onClick("test") } />
             <Square value = "3" onClick={() => onClick("test") } />
