a0a83454c380fa41850ea78c3f7886cf6db7470a
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-13 14:24:03 +0100

    Adds some styling to the Square component using inline styling

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const styling = {
+    background: 'pink',
+    fontSize: '45px',
+    fontWeight: '700',
+    border: '5px solid black',
+    cursor: 'pointer',
+    outline: 'none',
+    padding: '40px'
+}
 
 const Square = ({value, onClick}) => 
      (
-        <button onClick={onClick} >
+        <button style={styling} onClick={onClick} >
             {value}
         </button>
     )
